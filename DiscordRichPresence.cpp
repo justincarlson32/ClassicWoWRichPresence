9089ee8586ccc819db6bf15d8f36cbaf8e7e4f9f
Justin Carlson <41803725+justincarlson32@users.noreply.github.com>
2021-03-12 00:32:57 -0800

    Update DiscordRichPresence.cpp

--- DiscordRichPresence.cpp
+++ DiscordRichPresence.cpp
@@ -12,20 +12,6 @@
 
 /*
 
-__________                    ___________
-\____    /___________  ____   \__    ___/_  _  ______
-  /     // __ \_  __ \/  _ \    |    |  \ \/ \/ /  _ \
- /     /\  ___/|  | \(  <_> )   |    |   \     (  <_> )
-/_______ \___  >__|   \____/    |____|    \/\_/ \____/
-		\/   \/
-
-Copyright (C) 2019 Justin Carlson
-
-This piece of software is witheld under the Zero Two license
-which is basically just the GNU public licence. But you know...
-with more DARLING!!!
-
-
 This program is free software: you can redistribute it and/or modify 
 it under the terms of the GNU General Public License as published by 
 the Free Software Foundation, either version 3 of the License, or (at your option) 
@@ -36,21 +22,6 @@ without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 See the GNU General Public License for more details.
 
 You can find more details here: https://www.gnu.org/licenses/gpl-3.0.en.html
-
-⣿⣿⣿⣿⢡⣿⢇⠄⢨⣶⣿⣿⣿⣿⣿⣿⣷⣶⣭⣟⡿⠁⣸⣿⣿⣿⣿⣧⢣⢧
-⣿⣿⣿⡏⣿⢣⠾⣤⠞⣡⣶⣶⣶⣶⣬⣭⣽⣛⠻⣿⡇⠄⡛⢿⣿⣿⣿⣿⡞⣎
-⣿⣿⣸⣁⣵⣿⣶⠎⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⢱⡼⢼⣧⡹⣿⣿⣿⡇⣿
-⠄⣩⣵⣿⣿⢹⣿⢃⢳⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢳⣿⣧⣝⢻⡝⣿⣿⡇⣿
-⠃⣾⠃⣿⡏⢇⣮⣟⠸⡇⣿⣿⢸⠿⣿⣿⣤⢿⣿⣿⢸⢹⡆⡟⣸⣿⡟⣿⡇⣿
-⢰⣿⠄⣿⡇⡜⠛⠛⠿⢤⢹⣿⣼⢀⣿⡏⠿⠄⣿⠟⣰⣦⢧⢱⣿⣿⠳⣿⠃⣿
-⡜⣿⡆⣿⡇⣿⣿⣷⣶⣾⣦⣄⣧⣸⡸⢧⣿⡨⠩⠦⠿⠿⡼⢸⣿⡿⣄⢈⡆⢸
-⠹⣿⡇⣿⡇⣿⣿⣿⣿⣿⣿⣿⢻⣿⣿⣿⣶⣶⣶⣶⣶⣶⡆⣿⣿⢇⡟⣼⡧⣫
-⠄⢹⡇⣿⡇⣿⣿⣿⣿⣿⣿⣧⣤⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⣿⣿⢘⣾⣿⢷⠋
-⠄⠈⡇⣿⡇⣹⣿⣿⣿⣿⣿⣧⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢣⣿⡟⣾⣿⣿⠄⠄
-⠄⠄⢠⣿⡇⣷⠘⢿⣿⣧⡲⣾⣭⣭⣿⣒⠊⣹⣿⣿⡿⠁⣸⣿⡇⣿⣿⡏⡇⠄
-⠄⠄⠄⣿⡇⣿⠄⠄⠙⢿⣿⣷⣶⣶⣶⣾⣿⣿⠟⠋⠄⠄⣿⣿⢳⣿⣿⢹⡇⠄
-⠄⠄⠄⠘⣿⢸⢰⣆⠄⠄⠙⠻⣿⡿⠟⠛⠉⠄⠄⠄⠄⠄⣿⡟⣼⣿⢏⣿⢧⣷
-
 
 */
 
